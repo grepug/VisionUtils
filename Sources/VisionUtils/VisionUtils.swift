be8b6f7e7bfcb8cfdc28070f8d3d46149d0bc378
Kai Shao <grepug@icloud.com>
2022-04-06 11:07:37 +0800

    Initial Commit

---
+++ Sources/VisionUtils/VisionUtils.swift
@@ -0,0 +1,6 @@
+public struct VisionUtils {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
---
+++ Tests/VisionUtilsTests/VisionUtilsTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import VisionUtils
+
+final class VisionUtilsTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(VisionUtils().text, "Hello, World!")
+    }
+}
